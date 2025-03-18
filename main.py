d11fc93b874a2718614e3f60ceb2e2cd3135a8e1
Username387J <jdeleijagbulu@gmail.com>
2025-03-17 19:56:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def draw():
 
 def display_message(heading,subheading):
     screen.draw.text(heading, fontsize = 60, center=(400,300), color="black")
-    screen.draw.text(subheading, fontsize=30, center=(300,300), color="black")
+    screen.draw.text(subheading, fontsize=30, center=(400,330), color="black")
 
 def update():
     global items 
@@ -61,17 +61,59 @@ def get_option_to_create(number_of_extra_items):
 
 
 def create_items(items_to_create):
-    return []
+    new_items = []
+    for i in items_to_create:
+        item = Actor(i+ "img")
+        new_items.append(item)
+    return new_items
 
 
 def layout_items(items_to_layout):
-    pass 
+    number_of_gaps=len(items_to_layout)+1
+    gap_size=WIDTH/number_of_gaps
+    random.shuffle(items_to_layout)
+
+    for index,item in enumerate(items_to_layout):
+        new_x_pos = (index + 1) * gap_size
+        item.x = new_x_pos
 
 def animate_items(items_to_animate):
-    pass
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED-current_level
+        animation = animate(item,duration=duration, on_finished=handle_game_over, y=HEIGHT)
+        animations.append(animation)
 
-
+def handle_game_over():
+    global game_over
+    game_over=True
     
+def on_mouse_down(pos):
+    global items
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+
+def handle_game_complete():
+    global current_level, items, animations, game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level+=1
+        items = []
+        animations = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+
 
 pgzrun.go()
 
